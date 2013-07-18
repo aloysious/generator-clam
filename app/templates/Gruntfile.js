b20ee3bc3acbe88001df95d5e6191126dc3b2eee
拔赤 <jay.li@alibaba-inc.com>
2013-07-18 20:29:36 +0800

    commit byjakly

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -153,7 +153,7 @@ module.exports = function (grunt) {
          */
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+					 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd hh:MM:ss") %> */\n',
                 beautify: {
                     ascii_only: true
                 }
