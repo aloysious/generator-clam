416e61b9983edf772f42ca72b0144b6bcb3d3921
bachi <jay.li@alibaba-inc.com>
2013-07-27 16:46:24 +0800

    doc modified

---
+++ build/index.js
@@ -0,0 +1,49 @@
+// 'use strict';
+var util = require('util');
+var path = require('path');
+var ClamLogo = require('../app/logo').ClamLogo;
+var yeoman = require('yeoman-generator');
+var ABC = require('abc-generator');
+
+var AppGenerator = module.exports = function AppGenerator(args, options, config) {
+	// yeoman.generators.Base.apply(this, arguments);
+	ABC.UIBase.apply(this, arguments);
+
+    this.on('end', function () {
+		console.log(yellow('TODO !'));
+    }.bind(this));
+
+};
+
+util.inherits(AppGenerator, ABC.UIBase);
+
+AppGenerator.prototype.askFor = function askFor() {
+	var cb = this.async();
+	
+    // welcome message
+	console.log(ClamLogo(this));
+
+};
+
+function consoleColor(str,num){
+	if (!num) {
+		num = '32';
+	}
+	return "\033[" + num +"m" + str + "\033[0m"
+}
+
+function green(str){
+	return consoleColor(str,32);
+}
+
+function yellow(str){
+	return consoleColor(str,33);
+}
+
+function red(str){
+	return consoleColor(str,31);
+}
+
+function blue(str){
+	return consoleColor(str,34);
+}
